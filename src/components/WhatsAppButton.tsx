5da14f4e0c4a4ff2cc90d124ba68c10d82e45995
RubenVazquez <ruben.vazquez@workcube.com.mx>
2025-05-14 23:44:35 -0600

    Update | Topbar con item active, SideNav con item active, Sección con información.

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -11,7 +11,6 @@ const WhatsAppButton: FC = () => {
       className="bg-emerald-600 text-white px-4 py-2 rounded-md flex items-center space-x-2 font-medium hover:bg-emerald-700 transition-colors"
     >
       <i className="fab fa-whatsapp" />
-      <span>Contáctanos</span>
     </a>
   );
 };
